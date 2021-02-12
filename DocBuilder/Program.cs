d25b0161d20ee7b04d720d08a53ad478e4dc6002
aleshin-roman-nk <aleshin.roman.nk@gmail.com>
2021-02-12 12:07:01 +0700

    12-02-2021

---
+++ DocBuilder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocBuilder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string tempDoc = File.ReadAllText("contract.html");
+
+			var table = new Dictionary<string, string>();
+
+			string body = "Адрес %kv:<, кв kv>%";
+
+			table["kv"] = "123";
+
+
+			DBiuld doc = new DBiuld(table, body);
+			Console.WriteLine(doc.Document);
+
+			//File.WriteAllText("out.html", doc.Document, Encoding.UTF8);
+
+			var vars = DBiuld.AllVariables(tempDoc);
+
+			foreach (var item in vars)
+			{
+				Console.WriteLine(item);
+			}
+
+			//Console.WriteLine("Done!!!");
+			Console.ReadLine();
+		}
+	}
+}
